16d4c7d45696b5eafba0b15fc750d96648137dde
zhangyuang <leonin0829@gmail.com>
2020-03-23 10:14:40 +0800

    chore: update .gitignore

--- packages/ssr-cli/cjs/getCwd.d.ts
+++
@@ -1,2 +0,0 @@
-declare const getCwd: () => string;
-export { getCwd };
--- packages/ssr-cli/src/utils/getCwd.ts
+++ packages/ssr-cli/src/utils/getCwd.ts
@@ -1,7 +1,7 @@
 import { join, isAbsolute } from 'path'
 
 const getCwd = () => {
-  let cwd = process.cwd()
+  const cwd = process.cwd()
   if (process.env.APP_ROOT) {
     // avoid repeat cwd path
     if (!isAbsolute(process.env.APP_ROOT)) {
