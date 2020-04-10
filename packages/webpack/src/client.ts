2afb7520c7525308ba97550bc4f97a46b035e058
zhangyuang <leonin0829@gmail.com>
2020-04-10 15:45:58 +0800

    chore: make lerna publish

--- packages/webpack/src/client.ts
+++ packages/webpack/src/client.ts
@@ -18,6 +18,7 @@ const getClientWebpack = (argv: Argv) => {
   // @ts-ignore
   const { funcName } = argv.faasRoutes[0]
   const config = getBaseConfig()
+
   config.resolve.alias
                 .set('react-router', loadModule('react-router'))
                 .set('react-router-dom', loadModule('react-router-dom'))
