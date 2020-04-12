7efe3496cbda73f83342d13a5885b70dacf97ebc
zhangyuang <leonin0829@gmail.com>
2020-04-12 15:40:55 +0800

    chore: ssr-server-utils dependencies add shelljs

--- example/spa/web/config.js
+++ example/spa/web/config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-  // staticPrefix: 'https://g.alicdn.com/youku-node/shiyiCdn/1.0.8/build/index/client'
+  staticPrefix: 'https://g.alicdn.com/youku-node/shiyiCdn/1.0.9/build/index/client'
 }
