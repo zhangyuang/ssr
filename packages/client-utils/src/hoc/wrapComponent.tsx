3477575e6ab155d3651a7cdd57c1628f8b098ea7
zhangyuang <leonin0829@gmail.com>
2020-04-20 17:22:37 +0800

    chore: update wrapCompnent

--- packages/client-utils/src/hoc/wrapComponent.tsx
+++ packages/client-utils/src/hoc/wrapComponent.tsx
@@ -5,11 +5,9 @@ import { FC, IWindow } from 'ssr-types'
 declare const window: IWindow
 
 let _this: any = null
-let routerChanged = false
 
 const popStateFn = (e: PopStateEvent) => {
   // historyPop的时候需要调用fetch
-  routerChanged = true
   // 使用popStateFn保存函数防止addEventListener重复注册,排除hashchange的情况
   if (!location.hash && _this && _this.fetch) {
     _this.fetch()
@@ -27,9 +25,7 @@ function wrapComponent (WrappedComponent: FC): React.ComponentClass {
       this.state = {
         extraProps: {}
       }
-      if (!routerChanged) {
-        routerChanged = !window.__USE_SSR__ || (props.history && props.history.action === 'PUSH')
-      }
+
       if (window.__USE_SSR__) {
         _this = this // 修正_this指向，保证_this指向当前渲染的页面组件
         window.addEventListener('popstate', popStateFn)
@@ -37,13 +33,14 @@ function wrapComponent (WrappedComponent: FC): React.ComponentClass {
     }
 
     async componentDidMount () {
-      // history push的时候需要调用fetch
-      if (this.props.history && this.props.history.action !== 'POP' || !window.__USE_SSR__) {
+      // history push|replace 或者客户端渲染的时候需要调用fetch
+      if ((this.props.history && this.props.history.action !== 'POP') || !window.__INITIAL_DATA__) {
         await this.fetch()
       }
     }
 
     async fetch () {
+      delete window.__INITIAL_DATA__
       // csr首次进入页面以及csr/ssr切换路由时才调用fetch
       const props = this.props
       const extraProps = WrappedComponent.fetch ? await WrappedComponent.fetch(props) : {}
@@ -54,7 +51,7 @@ function wrapComponent (WrappedComponent: FC): React.ComponentClass {
 
     render () {
       // 只有在首次进入页面需要将window.__INITIAL_DATA__作为props，路由切换时不需要
-      return <WrappedComponent {...Object.assign({}, this.props, routerChanged ? {} : window.__INITIAL_DATA__, this.state.extraProps)} />
+      return <WrappedComponent {...Object.assign({}, this.props, window.__INITIAL_DATA__ || {}, this.state.extraProps)} />
     }
   }
   return withRouter(WrapComponentClass)
--- packages/client-utils/src/index.ts
+++ packages/client-utils/src/index.ts
@@ -3,6 +3,6 @@ export * from './hoc/wrapComponent'
 export * from './hoc/wrapLayout'
 export * from './hoc/onlyCsr'
 export * from './errorCatch'
-export * from './findRoute'
+
 /* types */
 export * from 'ssr-types'
