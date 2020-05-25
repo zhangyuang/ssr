7715f5fcdf3714bb6d6ade9e23992d0f40f19162
zhangyuang <leonin0829@gmail.com>
2020-05-25 16:39:40 +0800

    style: code format

--- packages/webpack/src/entry/server-entry.tsx
+++ packages/webpack/src/entry/server-entry.tsx
@@ -7,11 +7,13 @@ declare const __isBrowser__: boolean
 const { staticPrefix, cssOrder, jsOrder, isDev, devManifest } = buildConfig
 const feRoutes: FeRouteItem[] = require('ssr-cache/route')
 declare const defineStaticPrefix: string
+
 const serverRender = async (ctx: IFaaSContext, options: Options): Promise<React.ReactElement> => {
   const _staticPrefix = defineStaticPrefix || staticPrefix // 以process.env为最高优先级
   const routeItem = findRoute<FeRouteItem<any>>(feRoutes, ctx.req.path)
   const faasRouteItem = findRoute<FaasRouteItem>(options.faasRoutes, ctx.req.path)
   const { funcName, mode } = faasRouteItem
+
   const staticList = getStaticList(isDev, devManifest, _staticPrefix, funcName, cssOrder, jsOrder)
   if (!routeItem) {
     throw new Error('Component is Not Found')
@@ -19,6 +21,7 @@ const serverRender = async (ctx: IFaaSContext, options: Options): Promise<React.
 
   const Layout = wrapLayout(routeItem.layout, __isBrowser__)
   const Component = routeItem.component
+
   if (mode !== 'ssr' || ctx.query?.csr) {
     // 根据mode和query来决定当前渲染模式
     return <StaticRouter><Layout ctx={ctx} staticList={staticList} config={buildConfig}></Layout></StaticRouter>
