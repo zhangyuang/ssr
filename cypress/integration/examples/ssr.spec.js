d452c60157cf26b731d240dfc326232fdb5e617f
zhangyuang <leonin0829@gmail.com>
2021-02-18 18:56:55 +0800

    fix e2e

--- cypress/integration/examples/ssr.spec.js
+++ cypress/integration/examples/ssr.spec.js
@@ -27,10 +27,10 @@ describe('SSR', () => {
     // context 状态能够被各个组件共享
     const text = 'Hello, World'
     cy.visit('http://localhost:3000')
-    cy.get('input').type(text)
+    cy.get('input').type(text, { force: true })
     cy.get('.swiper-slide-active').first().click()
     cy.get('input').should('have.value', text)
-    cy.get('input').type('new')
+    cy.get('input').type('new', { force: true })
     cy.go('back')
     cy.get('input').should('have.value', text + 'new')
   })
