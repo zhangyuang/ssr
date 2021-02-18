deb34468ff099d5e879944c82ad83f7546b00d9c
zhangyuang <leonin0829@gmail.com>
2021-02-18 23:21:49 +0800

    chore: update e2e

--- README.md
+++ README.md
@@ -46,7 +46,7 @@
 - 🌱　极易定制：前端支持 React/Vue 等现代Web框架；
 - 🚀　开箱即用：内置 10+ 脚手架配套扩展，如Antd、TS、Hooks等；
 - 🧲　插件驱动：基于插件架构，用户更加专注于业务逻辑；
-- 💯　Serverless优先：一键发布到各种Serverless平台，也支持传统Web Server，比如Egg、Midway、Nest和Next等。
+- 💯　Serverless优先：一键发布到各种Serverless平台，也支持传统Web Server，比如Egg、Midway、Nest等。
 - 🛡　高可用场景，可无缝从SSR降级到CSR，最佳容灾方案。
 
 ## 已实现的功能
--- cypress/integration/examples/ssr.spec.js
+++ cypress/integration/examples/ssr.spec.js
@@ -27,11 +27,11 @@ describe('SSR', () => {
     // context 状态能够被各个组件共享
     const text = 'Hello, World'
     cy.visit('http://localhost:3000')
-    cy.get('input').type(text, { force: true })
+    cy.get('.input').type(text, { force: true })
     cy.get('.swiper-slide-active').first().click()
-    cy.get('input').should('have.value', text)
-    cy.get('input').type('new', { force: true })
+    cy.get('.input').should('have.value', text)
+    cy.get('.input').type('new', { force: true })
     cy.go('back')
-    cy.get('input').should('have.value', text + 'new')
+    cy.get('.input').should('have.value', text + 'new')
   })
 })
