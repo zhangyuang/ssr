0ed7ad64e665e17e982085b2d712df88b5df3b7e
zhangyuang <leonin0829@gmail.com>
2021-02-24 19:33:06 +0800

    fix: nestjs example

--- example/nestjs-react-ssr/src/main.ts
+++ example/nestjs-react-ssr/src/main.ts
@@ -1,12 +1,17 @@
+import { join } from 'path'
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { initialSSRDevProxy } from 'ssr-server-utils'
+
 import { AppModule } from './module/app'
 
-async function bootstrap () {
-  const app = await NestFactory.create(AppModule)
+async function bootstrap (): Promise<void> {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   initialSSRDevProxy(app, {
     express: true
   })
+  app.useStaticAssets(join(process.cwd(), './build'))
+
   await app.listen(3000)
 }
 // eslint-disable-next-line
--- example/nestjs-react-ssr/src/module/app.ts
+++ example/nestjs-react-ssr/src/module/app.ts
@@ -1,6 +1,4 @@
-import { join } from 'path'
 import { Module } from '@nestjs/common'
-import { ServeStaticModule } from '@nestjs/serve-static'
 import { AppController } from '../controller/app'
 import { ApiController } from '../controller/api'
 import { ApiService } from '../service/index'
@@ -8,9 +6,7 @@ import { ApiDetailService } from '../service/detail'
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), './build')
-    })
+
   ],
   controllers: [AppController, ApiController],
   providers: [ApiService, ApiDetailService]
