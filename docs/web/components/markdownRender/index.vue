c01fcb99e5fe56a7d2780fce15568ff461109dbb
zhangyuang <leonin0829@gmail.com>
2021-04-19 18:51:16 +0800

    update

--- docs/web/components/markdownRender/index.vue
+++ docs/web/components/markdownRender/index.vue
@@ -48,7 +48,9 @@ export default defineComponent({
         highlight: function (str, lang) {
           if (lang && hljs.getLanguage(lang)) {
             try {
-              return `<pre class="hljs"><code>${hljs.highlight(lang, str, true).value}</code></pre>`
+              return `<pre class="hljs"><code>${hljs.highlight(str, {
+                language: lang
+              }).value}</code></pre>`
             } catch (__) {}
           }
           return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>'
