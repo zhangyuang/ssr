5405f1b44002d6124b540ce81f114337401d88da
zhangyuang <leonin0829@gmail.com>
2021-04-19 20:56:32 +0800

    chore: update config

--- docs/web/pages/docs/config.ts
+++ docs/web/pages/docs/config.ts
@@ -4,24 +4,19 @@ export default [
     path: 'why'
   },
   {
-    title: 'Basic Features',
+    title: '指引',
     routes: [
-      {
-        title: 'Pages',
-        path: 'basic-features$pages'
-      },
-      {
-        title: 'Data Fetching',
-        path: 'basic-features$data-fetching'
-      },
-      {
-        title: 'Built-in CSS Support',
-        path: 'basic-features$built-in-css-support'
-      },
-      {
-        title: 'Image Optimization',
-        path: 'basic-features$image-optimization'
-      }
+
+      { title: '快速开始', path: 'features$started' },
+      { title: '插件机制 ', path: 'features$plugin' },
+      { title: '约定式路由', path: 'features$feRoutes' },
+      { title: '本地开发 ', path: 'features$develop' },
+      { title: '组件通信 ', path: 'features$communication' },
+      { title: '使用Vite ', path: 'features$vite' },
+      { title: '应用部署 ', path: 'features$deploy' },
+      { title: '渲染降级', path: 'features$csr' },
+      { title: 'FAQ', path: 'features$faq' }
+
     ]
   },
   {
