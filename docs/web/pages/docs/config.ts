53b4f4eee53db5c2550e594bbd92cd8dc2cd99fe
zhangyuang <leonin0829@gmail.com>
2021-04-20 14:16:55 +0800

    docs: update demo.md

--- docs/web/pages/docs/config.ts
+++ docs/web/pages/docs/config.ts
@@ -7,21 +7,25 @@ export default [
     title: '指引',
     routes: [
       { title: '快速开始', path: 'features$started' },
-      { title: '插件机制 ', path: 'features$plugin' },
-      { title: '约定式路由', path: 'features$feRoutes' },
+      { title: '在线案例', path: 'features$demo' },
+      { title: 'Serverless', path: 'features$serverless' },
+      { title: '目录结构', path: 'features$structure' },
       { title: '本地开发 ', path: 'features$develop' },
+      { title: '约定式路由', path: 'features$feRoutes' },
       { title: '数据获取 ', path: 'features$fetch' },
       { title: '组件通信 ', path: 'features$communication' },
       { title: '使用Vite ', path: 'features$vite' },
+      { title: '插件机制 ', path: 'features$plugin' },
       { title: '应用部署 ', path: 'features$deploy' },
       { title: '渲染降级', path: 'features$csr' },
-      { title: 'FAQ', path: 'features$faq' }
+      { title: 'FAQ', path: 'features$faq' },
+      { title: '问题反馈', path: 'features$issue' }
     ]
   },
   {
     title: 'API',
     routes: [
-      
+
     ]
   }
 ]
