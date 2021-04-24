c6c2cb53b7ef3c62c3989f4f4153b13f30939f31
zhangyuang <leonin0829@gmail.com>
2021-04-25 01:14:40 +0800

    docs: update img

--- docs/src/service/detail.ts
+++
@@ -1,9 +0,0 @@
-import { Provide } from '@midwayjs/decorator'
-import mock from '../mock/detail'
-
-@Provide('ApiDetailService')
-export class ApiDetailService {
-  async index (id): Promise<any> {
-    return await Promise.resolve(mock.data[id])
-  }
-}
--- docs/src/service/index.ts
+++
@@ -1,9 +0,0 @@
-import { Provide } from '@midwayjs/decorator'
-import mock from '../mock'
-
-@Provide('ApiService')
-export class ApiIndexService {
-  async index (): Promise<any> {
-    return await Promise.resolve(mock)
-  }
-}
