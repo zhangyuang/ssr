3609d7fa58b54e6c6511f2d46f42b64a1f27ef66
zhangyuang <leonin0829@gmail.com>
2021-04-24 22:47:04 +0800

    docs: update img

--- docs/web/components/layout/index.vue
+++ docs/web/components/layout/index.vue
@@ -11,7 +11,7 @@
       <meta name="keywords" itemprop="keywords" content="服务端渲染, React, Egg, Midway, Nestjs, Vue, VueSSR, SSR, ReactSSR">
       <meta property="og:title" content="Vue, React, SSR 服务端渲染">
       <meta property="og:description" content="Vue, React, SSR 服务端渲染">
-      <title>ssr framework docs</title>
+      <title>ssr 框架官方文档</title>
       <slot name="viteClient" />
       <!-- 用于通过配置插入自定义的 script 为了避免影响期望功能这块内容不做 escape，为了避免 xss 需要保证插入脚本代码的安全性  -->
       <slot name="customeHeadScript" />
