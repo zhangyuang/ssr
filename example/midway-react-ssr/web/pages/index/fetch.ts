3b017a36a9a4bb27ede7be4cc83d52babce56fe3
zhangyuang <leonin0829@gmail.com>
2021-04-27 18:12:44 +0800

    chore: update example comments

--- example/midway-react-ssr/web/pages/index/fetch.ts
+++ example/midway-react-ssr/web/pages/index/fetch.ts
@@ -7,6 +7,7 @@ interface IApiService {
 export default async (ctx: ISSRContext<{
   apiService?: IApiService
 }>) => {
+  // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
   const data = __isBrowser__ ? await (await window.fetch('/api/index')).json() : await ctx.apiService?.index()
   return {
     // 建议根据模块给数据加上 namespace防止数据覆盖
--- example/nestjs-vue3-ssr/web/pages/index/fetch.ts
+++ example/nestjs-vue3-ssr/web/pages/index/fetch.ts
@@ -7,6 +7,8 @@ interface IApiService {
 export default async ({ store, router }, ctx?: ISSRContext<{
   apiService?: IApiService
 }>) => {
+  // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
+
   const data = __isBrowser__ ? await (await window.fetch('/api/index')).json() : await ctx?.apiService?.index()
   await store.dispatch('indexStore/initialData', { payload: data })
 }
