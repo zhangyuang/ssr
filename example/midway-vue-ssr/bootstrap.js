e6c102c67e1098da2738d7bff08db44ce4b646aa
zhangyuang <leonin0829@gmail.com>
2021-05-10 17:38:07 +0800

    chore: 删除无用文件

--- example/midway-vue-ssr/bootstrap.js
+++
@@ -1,7 +0,0 @@
-const WebFramework = require('@midwayjs/web').Framework
-const web = new WebFramework().configure({
-  port: 3000
-})
-
-const { Bootstrap } = require('@midwayjs/bootstrap')
-Bootstrap.load(web).run()
