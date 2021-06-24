ded036428befe216091599956e9666d267a2a442
zhangyuang <leonin0829@gmail.com>
2021-06-24 12:08:43 +0800

    chore: update example

--- example/nestjs-vue3-ssr/web/pages/detail/fetch.ts
+++ example/nestjs-vue3-ssr/web/pages/detail/fetch.ts
@@ -9,6 +9,6 @@ export default async ({ store, router }, ctx?: ISSRContext<{
 }>) => {
   // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
 
-  const data = __isBrowser__ ? await (await window.fetch(`/api/detail/${router.value ? router.value.params.id : router.params.id}`)).json() : await ctx?.apiDeatilservice?.index(ctx.request.params.id)
+  const data = __isBrowser__ ? await (await window.fetch(`/api/detail/${router.params.id}`)).json() : await ctx?.apiDeatilservice?.index(ctx.request.params.id)
   await store.dispatch('detailStore/initialData', { payload: data })
 }
