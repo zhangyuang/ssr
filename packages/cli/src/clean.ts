bfa625cf482b82ee4c690202b5097a06b2aaeff7
zhangyuang <leonin0829@gmail.com>
2021-07-02 16:09:10 +0800

    fix: update tsconfig.json check

--- packages/cli/src/clean.ts
+++ packages/cli/src/clean.ts
@@ -7,7 +7,12 @@ export const cleanOutDir = async () => {
   const cwd = getCwd()
   const tsconfigExist = await accessFile(resolve(cwd, './tsconfig.json'))
   if (tsconfigExist && process.env.CLEAN !== 'false') {
-    const outDir = require(resolve(cwd, './tsconfig.json')).compilerOptions.outDir
-    rm('-rf', resolve(cwd, outDir))
+    try {
+      const outDir = require(resolve(cwd, './tsconfig.json')).compilerOptions.outDir
+      rm('-rf', resolve(cwd, outDir))
+    } catch (error) {
+      // 有可能 json 文件存在注释导致 require 失败，这里 catch 一下
+      console.log('检测到当前目录 tsconfig.json 文件可能存在语法错误')
+    }
   }
 }
