8e2fbaf87c6fcb9d4ee4fbc503bc09c35c1cd932
zhangyuang <leonin0829@gmail.com>
2021-07-11 23:52:29 +0800

    chore: update axios config

--- packages/server-utils/src/manifest.tsx
+++ packages/server-utils/src/manifest.tsx
@@ -3,6 +3,12 @@ import axios from 'axios'
 import { getCwd } from './cwd'
 import { loadConfig } from './loadConfig'
 
+// 创建一个实例来请求，防止业务代码的 axios 设置了 defaults 配置导致获取 manifest 失败
+const instance = axios.create({
+  timeout: 3000,
+  proxy: false
+})
+
 let manifest: {
   [key: string]: string
 } = {}
@@ -15,7 +21,7 @@ const getManiFest = async () => {
   }
   const cwd = getCwd()
   if (isDev) {
-    const res = await axios.get(`${https ? 'https' : 'http'}://localhost:${fePort}/asset-manifest.json`)
+    const res = await instance.get(`${https ? 'https' : 'http'}://localhost:${fePort}/asset-manifest.json`)
     manifest = res.data
   } else {
     manifest = require(join(cwd, './build/client/asset-manifest.json'))
