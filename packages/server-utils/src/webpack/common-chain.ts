3baad157585aa441b4f3ecda332fc0c327730248
zhangyuang <leonin0829@gmail.com>
2021-07-30 16:51:13 +0800

    fix: url-loader limit

--- packages/server-utils/src/webpack/common-chain.ts
+++ packages/server-utils/src/webpack/common-chain.ts
@@ -11,7 +11,6 @@ const addImageChain = (chain: WebpackChain, isServer: boolean) => {
     .use('url-loader')
     .loader(loadModule('url-loader'))
     .options({
-      limit: false, // 关闭 base64
       name: '[name].[hash:8].[ext]',
       // require 图片的时候不用加 .default
       esModule: false,
