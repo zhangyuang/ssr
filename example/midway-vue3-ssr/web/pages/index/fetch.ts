a941a94b0f36c910601fcf84e07c677fe5e2e12e
zhangyuang <leonin0829@gmail.com>
2021-08-05 00:25:59 +0800

    chore: update example

--- example/midway-vue3-ssr/web/pages/index/fetch.ts
+++ example/midway-vue3-ssr/web/pages/index/fetch.ts
@@ -1,10 +1,16 @@
+import { Store } from 'vuex'
+import { RouteLocationNormalizedLoaded } from 'vue-router'
 import { ISSRContext } from 'ssr-types'
 import { IndexData } from '@/interface'
 interface IApiService {
   index: () => Promise<IndexData>
 }
+interface Params {
+  store: Store<any>
+  router: RouteLocationNormalizedLoaded
+}
 
-export default async ({ store, router }, ctx?: ISSRContext<{
+export default async ({ store, router }: Params, ctx?: ISSRContext<{
   apiService?: IApiService
 }>) => {
   // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
--- example/nestjs-vue-ssr/web/pages/detail/fetch.ts
+++ example/nestjs-vue-ssr/web/pages/detail/fetch.ts
@@ -1,11 +1,16 @@
+import { Store } from 'vuex'
+import { Route } from 'vue-router'
 import { ISSRContext } from 'ssr-types'
 import { IndexData } from '@/interface'
 
 interface ApiDeatilservice {
   index: (params: any) => Promise<IndexData>
 }
-
-export default async ({ store, router }, ctx?: ISSRContext<{
+interface Params {
+  store: Store<any>
+  router: Route
+}
+export default async ({ store, router }: Params, ctx?: ISSRContext<{
   apiDeatilservice?: ApiDeatilservice
 }>) => {
   // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
