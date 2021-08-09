b6e5666aaa8aa45923b5f9f9bbc1cabef94fc244
zhangyuang <leonin0829@gmail.com>
2021-08-09 21:07:12 +0800

    fix: stdout color force in child_process

--- packages/plugin-nestjs/src/start.ts
+++ packages/plugin-nestjs/src/start.ts
@@ -6,7 +6,9 @@ const spinner = require('ora')('starting ')
 const start = () => {
   const config = loadConfig()
   spinner.start()
-  const { stdout, stderr } = exec('npx nest start --watch', {} /* options, [optional] */)
+  const { stdout, stderr } = exec('npx nest start --watch', {
+    env: { ...process.env, FORCE_COLOR: '1' }
+  } /* options, [optional] */)
   stdout?.on('data', function (data) {
     console.log(data)
     if (data.match('Nest application successfully started')) {
