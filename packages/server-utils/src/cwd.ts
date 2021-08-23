16eba75a9d35effe700ab9ef2722726ef7602dca
zhangyuang <leonin0829@gmail.com>
2021-08-23 20:30:50 +0800

    chore: update tips

--- packages/server-utils/src/cwd.ts
+++ packages/server-utils/src/cwd.ts
@@ -103,7 +103,7 @@ const checkVite = async () => {
     } else {
       plugin = '@vitejs/plugin-react-refresh'
     }
-    console.log(`当前项目缺少 vite 依赖，请根据实际技术栈安装 vite ${plugin} 或 其他对应插件`)
+    console.log(`当前项目缺少 vite 依赖，请根据实际技术栈安装 vite ${plugin}${version && !/^.?3/.test(version) ? '@vite-plugin-vue2@1.4.4' : ''} 或 其他对应插件`)
     if (version && !/^.?3/.test(version)) {
       console.log('vue2 场景下使用 Vite 必须安装固定版本 vite-plugin-vue2@1.4.4')
     }
@@ -117,7 +117,7 @@ const copyViteConfig = async () => {
   const result = await accessFile(resolve(getCwd(), './vite.config.js'))
   if (!result) {
     const version = require(resolve(getCwd(), './package.json')).dependencies.vue
-    console.log('vite.config.js is not found, will be created automatically')
+    console.log('vite.config.js not found, will be created automatically')
     let folder = ''
     if (version) {
       folder = /^.?3/.test(version) ? 'ssr-plugin-vue3' : 'ssr-plugin-vue'
