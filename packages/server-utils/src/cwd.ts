016928569d685bdb6fd2f68dccebd118f7d8dc2c
zhangyuang <leonin0829@gmail.com>
2021-08-24 13:20:45 +0800

    chore: update tips

--- packages/server-utils/src/cwd.ts
+++ packages/server-utils/src/cwd.ts
@@ -103,7 +103,7 @@ const checkVite = async () => {
     } else {
       plugin = '@vitejs/plugin-react-refresh'
     }
-    console.log(`当前项目缺少 vite 依赖，请根据实际技术栈安装 vite ${plugin}${version && !/^.?3/.test(version) ? '@vite-plugin-vue2@1.4.4' : ''} 或 其他对应插件`)
+    console.log(`当前项目缺少 vite 依赖，请根据实际技术栈安装 vite ${plugin}${version && !/^.?3/.test(version) ? '@1.4.4' : ''} 或 其他对应插件`)
     if (version && !/^.?3/.test(version)) {
       console.log('vue2 场景下使用 Vite 必须安装固定版本 vite-plugin-vue2@1.4.4')
     }
