9f30211c4bd8bc50af7d4a78bc077cea3882cb26
zhangyuang <leonin0829@gmail.com>
2021-09-14 15:58:37 +0800

    test: update e2e spec

--- e2e/support/index.js
+++ e2e/support/index.js
@@ -27,6 +27,5 @@ Cypress.on('window:before:load', (win) => {
 afterEach(() => {
   cy.window().then((win) => {
     expect(win.console.error).to.have.callCount(0)
-    expect(win.console.warn).to.have.callCount().to.be.lessThan(2)
   })
 })
