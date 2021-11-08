853482e5abf085d22b439d961a30ac2703e3e59f
zhangyuang <leonin0829@gmail.com>
2021-11-08 20:16:04 +0800

    chore: update

--- packages/plugin-vue/src/entry/create.ts
+++ packages/plugin-vue/src/entry/create.ts
@@ -5,6 +5,7 @@ import * as Router from 'vue-router'
 
 import { FeRoutes, store } from '_build/ssr-temporary-routes'
 import { VueRouterOptions } from './interface'
+import { deepClone } from './clone'
 
 // without tsconfig esModuleInterop options must use the compatible syntax
 const RealVue = Vue.default || Vue
@@ -13,48 +14,6 @@ const RealVuex = Vuex.default || Vuex
 
 RealVue.use(RealRouter)
 RealVue.use(RealVuex)
-
-function deepClone (item) {
-  if (!item) { return item } // null, undefined values check
-
-  var types = [Number, String, Boolean]
-  var result
-
-  // normalizing primitives if someone did new String('aaa'), or new Number('444');
-  types.forEach(function (type) {
-    if (item instanceof type) {
-      result = type(item)
-    }
-  })
-
-  if (typeof result === 'undefined') {
-    if (Object.prototype.toString.call(item) === '[object Array]') {
-      result = []
-      item.forEach(function (child, index, array) {
-        result[index] = deepClone(child)
-      })
-    } else if (typeof item === 'object') {
-      if (!item.prototype) { // check that this is a literal
-        if (item instanceof Date) {
-          result = new Date(item)
-        } else {
-          // it is an object literal
-          result = {}
-          for (var i in item) {
-            // eslint-disable-next-line
-            result[i] = deepClone(item[i])
-          }
-        }
-      } else {
-        result = item
-      }
-    } else {
-      result = item
-    }
-  }
-
-  return result
-}
 
 function createRouter (options: VueRouterOptions = {}): Router {
   return new RealRouter({
