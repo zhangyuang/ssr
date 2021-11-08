60c8a478938df7c06584595294bdb470fadc126b
zhangyuang <leonin0829@gmail.com>
2021-11-08 20:55:27 +0800

    chore: update example

--- example/midway-vue3-ssr/src/service/detail.ts
+++ example/midway-vue3-ssr/src/service/detail.ts
@@ -1,9 +1,10 @@
 import { Provide } from '@midwayjs/decorator'
+import { Ddata } from '~/typings/data'
 import mock from '../mock/detail'
 
 @Provide('ApiDetailService')
 export class ApiDetailService {
-  async index (id): Promise<any> {
+  async index (id): Promise<Ddata> {
     return await Promise.resolve(mock.data[id])
   }
 }
