04cc23c54249d91deac0b62e44e9f81fe6133bf4
zhangyuang <leonin0829@gmail.com>
2021-11-08 19:55:39 +0800

    fix: add deepClone in createStore

--- packages/plugin-vue/src/entry/create.ts
+++ packages/plugin-vue/src/entry/create.ts
@@ -14,6 +14,48 @@ const RealVuex = Vuex.default || Vuex
 RealVue.use(RealRouter)
 RealVue.use(RealVuex)
 
+function deepClone (item) {
+  if (!item) { return item } // null, undefined values check
+
+  var types = [Number, String, Boolean]
+  var result
+
+  // normalizing primitives if someone did new String('aaa'), or new Number('444');
+  types.forEach(function (type) {
+    if (item instanceof type) {
+      result = type(item)
+    }
+  })
+
+  if (typeof result === 'undefined') {
+    if (Object.prototype.toString.call(item) === '[object Array]') {
+      result = []
+      item.forEach(function (child, index, array) {
+        result[index] = deepClone(child)
+      })
+    } else if (typeof item === 'object') {
+      if (!item.prototype) { // check that this is a literal
+        if (item instanceof Date) {
+          result = new Date(item)
+        } else {
+          // it is an object literal
+          result = {}
+          for (var i in item) {
+            // eslint-disable-next-line
+            result[i] = deepClone(item[i])
+          }
+        }
+      } else {
+        result = item
+      }
+    } else {
+      result = item
+    }
+  }
+
+  return result
+}
+
 function createRouter (options: VueRouterOptions = {}): Router {
   return new RealRouter({
     mode: 'history',
@@ -23,7 +65,7 @@ function createRouter (options: VueRouterOptions = {}): Router {
 }
 
 function createStore () {
-  return new Vuex.Store(store ?? {})
+  return new Vuex.Store(deepClone(store) ?? {})
 }
 
 export {
--- packages/plugin-vue3/src/entry/create.ts
+++ packages/plugin-vue3/src/entry/create.ts
@@ -1,4 +1,4 @@
-
+// @ts-nocheck
 import { createRouter as create, createWebHistory, createMemoryHistory } from 'vue-router'
 import { createStore as createVuexStore } from 'vuex'
 // @ts-expect-error
@@ -7,6 +7,48 @@ import * as Routes from '_build/ssr-temporary-routes'
 import { RoutesType, VueRouterOptions } from './interface'
 
 const { store, FeRoutes } = Routes as RoutesType
+
+function deepClone (item) {
+  if (!item) { return item } // null, undefined values check
+
+  var types = [Number, String, Boolean]
+  var result
+
+  // normalizing primitives if someone did new String('aaa'), or new Number('444');
+  types.forEach(function (type) {
+    if (item instanceof type) {
+      result = type(item)
+    }
+  })
+
+  if (typeof result === 'undefined') {
+    if (Object.prototype.toString.call(item) === '[object Array]') {
+      result = []
+      item.forEach(function (child, index, array) {
+        result[index] = deepClone(child)
+      })
+    } else if (typeof item === 'object') {
+      if (!item.prototype) { // check that this is a literal
+        if (item instanceof Date) {
+          result = new Date(item)
+        } else {
+          // it is an object literal
+          result = {}
+          for (var i in item) {
+            // eslint-disable-next-line
+            result[i] = deepClone(item[i])
+          }
+        }
+      } else {
+        result = item
+      }
+    } else {
+      result = item
+    }
+  }
+
+  return result
+}
 
 function createRouter (options: VueRouterOptions = {}) {
   const base = options.base ?? '/'
@@ -18,7 +60,7 @@ function createRouter (options: VueRouterOptions = {}) {
 }
 
 function createStore () {
-  return createVuexStore(store ?? {})
+  return createVuexStore(deepClone(store) ?? {})
 }
 
 export {
