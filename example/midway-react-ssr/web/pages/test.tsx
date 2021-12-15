3fcf71cc645b04f2703b592607099a096fe82961
zhangyuang <leonin0829@gmail.com>
2021-12-16 01:05:33 +0800

    ci: test

--- example/midway-react-ssr/web/pages/test.tsx
+++ example/midway-react-ssr/web/pages/test.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import type { IData } from '~/typings/data'
 
 export default function Test (props: IData) {
+  const foo = { state: { bar: 1 } }
   return (
     <div>
     Test
+      {foo?.state?.bar}
     </div>
   )
 }
