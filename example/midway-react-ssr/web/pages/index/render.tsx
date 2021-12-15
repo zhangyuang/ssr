9e5beafd70147df71c7e03f88b4a9ee702bded5c
zhangyuang <leonin0829@gmail.com>
2021-12-15 22:59:34 +0800

    ci: test

--- example/midway-react-ssr/web/pages/index/render.tsx
+++ example/midway-react-ssr/web/pages/index/render.tsx
@@ -7,10 +7,16 @@ import Search from '@/components/search'
 import { STORE_CONTEXT } from '_build/create-context'
 
 export default function Index (props: SProps) {
-  // const { state, dispatch } = useContext<IContext<{}>>(STORE_CONTEXT)
+  const { state, dispatch } = useContext<IContext<{}>>(STORE_CONTEXT)
   return (
     <div>
-      213
+      <Search></Search>
+      {
+        state?.indexData?.data?.[0]?.components ? <div>
+          <Slider {...props} data={state.indexData.data[0].components} />
+          <Rectangle {...props} data={state.indexData.data[1].components} />
+        </div> : <img src='https://gw.alicdn.com/tfs/TB1v.zIE7T2gK0jSZPcXXcKkpXa-128-128.gif' className='loading' />
+      }
     </div>
   )
 
