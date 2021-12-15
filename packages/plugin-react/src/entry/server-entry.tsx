af3100a74d47ffcc9af2800caef22dbb79c7a4ba
zhangyuang <leonin0829@gmail.com>
2021-12-15 15:48:50 +0800

    fix: local test vite

--- packages/plugin-react/src/entry/server-entry.tsx
+++ packages/plugin-react/src/entry/server-entry.tsx
@@ -81,7 +81,7 @@ const serverRender = async (ctx: ISSRContext, config: IConfig): Promise<React.Re
   const isCsr = !!(mode === 'csr' || ctx.request.query?.csr)
   const { component, fetch } = routeItem
   const Component = isCsr ? React.Fragment : (await component()).default
-  console.log('xxx', FeRoutes, routeItem)
+  console.log('xxx', component, (await component()).default)
   if (isCsr) {
     logGreen(`Current path ${path} use csr render mode`)
   }
