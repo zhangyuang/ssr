0bb320deb948131baac90963371969fe8a051c9d
zhangyuang <leonin0829@gmail.com>
2022-01-06 19:20:36 +0800

    feat: update ssr-types remove egg

--- packages/types/src/ctx.ts
+++ packages/types/src/ctx.ts
@@ -1,30 +1,29 @@
-import { Request, Response } from 'express'
-import { Context } from 'egg'
+import type { Request, Response } from 'express'
+import type { RouterContext } from 'koa-router'
+import type Cookies from 'cookies'
 
 export interface ExpressContext {
   request: Request
   response: Response
 }
 
-export type ISSRContext<T={}> = (ExpressContext|Context) & T
+// compatible with egg types
+type IKoaContext = Omit<RouterContext, 'cookies' |'router'| '_matchedRoute'| '_matchedRouteName'> & {
+  cookies: Partial<Cookies>
+}
+
+export type ISSRContext<T={}> = (ExpressContext|IKoaContext) & T
 export type ISSRNestContext<T={}> = ExpressContext & T
-export type ISSRMidwayContext<T={}> = Context & T
+export type ISSRMidwayContext<T={}> = IKoaContext & T
 
 export interface Options {
   mode?: string
 }
-export interface IWindow extends Window {
+export interface IWindow {
   __USE_SSR__?: boolean
   __INITIAL_DATA__?: any
   STORE_CONTEXT?: any
   __USE_VITE__?: boolean
   __disableClientRender__?: boolean
   prefix?: string
-}
-
-export interface IGlobal {
-  window: {
-    __USE_SSR__?: IWindow['__USE_SSR__']
-    __INITIAL_DATA__?: IWindow['__INITIAL_DATA__']
-  }
 }
