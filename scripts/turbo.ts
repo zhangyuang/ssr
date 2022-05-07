116673d94b8be45a5d1b06f0c8f26b90a7979fc0
Zack <mwl@live.com>
2022-05-07 14:56:01 +0800

    feat: add `turbo`

---
+++ scripts/turbo.ts
@@ -0,0 +1,44 @@
+import yArgs from 'yargs-parser';
+import { spawnSync } from './utils';
+
+(async () => {
+  const args = yArgs(process.argv.slice(2));
+  const filter = args.filter || './packages/*';
+  const extra = (args._ || []).join(' ');
+
+  turbo({
+    cmd: args.cmd,
+    filter,
+    extra,
+    cache: args.cache,
+    parallel: args.parallel,
+  });
+})();
+
+function turbo(opts: {
+  filter: string;
+  cmd: string;
+  extra?: string;
+  cache?: boolean;
+  parallel?: boolean;
+}) {
+  const extraCmd = opts.extra ? `-- -- ${opts.extra}` : '';
+  const cacheCmd = opts.cache === false ? '--no-cache --force' : '';
+  const parallelCmd = opts.parallel ? '--parallel' : '';
+
+  const options = [
+    opts.cmd,
+    `--cache-dir=".turbo"`,
+    `--filter="${opts.filter}"`,
+    cacheCmd,
+    parallelCmd,
+    extraCmd,
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  const command = `turbo run ${options}`;
+  spawnSync(command, {
+    cwd: process.cwd(),
+  });
+}
