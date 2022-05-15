f3ab55d95bfcd56f7a2b397243a50d1a5fe84096
zhangyuang <leonin0829@gmail.com>
2022-05-15 14:57:28 +0800

    chore: update example

--- example/midway-vue3-ssr/src/config/config.default.ts
+++ example/midway-vue3-ssr/src/config/config.default.ts
@@ -2,8 +2,5 @@ import { MidwayConfig } from '@midwayjs/core'
 
 export default {
   // use for cookie sign key, should change to your own and keep security
-  keys: '1650192482948_2252',
-  koa: {
-    port: 3000
-  }
+  keys: '1650192482948_2252'
 } as MidwayConfig
