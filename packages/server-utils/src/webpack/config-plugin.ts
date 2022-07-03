aa5ca6bc8b843b3f4d2991259ad7004b4b3cf291
zhangyuang <leonin0829@gmail.com>
2022-07-03 15:48:07 +0800

    chore: update skip ci

--- packages/server-utils/src/webpack/config-plugin.ts
+++ packages/server-utils/src/webpack/config-plugin.ts
@@ -8,9 +8,16 @@ const pageMap: Record<string, string> = {}
 const dependenciesMap: Record<string, string[]> = {}
 const pageChunkRe = /['"](.*)?['"]/
 
+const normalizePath = (path: string) => {
+  const { alias } = loadConfig()
+  for (const a in alias) {
+    path = path.replace(a, alias[a])
+  }
+  return path
+}
+
 export class WebpackChunkNamePlugin implements WebpackPluginInstance {
-  apply(compiler: Compiler) {
-    const { alias } = loadConfig()
+  apply (compiler: Compiler) {
     compiler.hooks.compilation.tap(
       'ChunkNamePlugin',
       (compilation) => {
@@ -24,9 +31,7 @@ export class WebpackChunkNamePlugin implements WebpackPluginInstance {
                 const { s: start, e: end } = imports[index]
                 let rawUrl: string = content.slice(start, end)
                 if (!rawUrl.includes('render')) continue
-                for (const a in alias) {
-                  rawUrl = rawUrl.replace(a, alias[a])
-                }
+                rawUrl = normalizePath(rawUrl)
                 const chunkName = webpackCommentRegExp.exec(rawUrl)![1]
                 rawUrl = rawUrl.replace(/\/\*(.*)?\*\//, '')
                 const originUrl = pageChunkRe.exec(rawUrl)![1]
@@ -47,50 +52,39 @@ const checkOrigin = (request: string) => {
   }
   return false
 }
+
+const getDependenciesPath = (module) => {
+  if (!module.request) return 'void'
+  const { request, originModule } = module
+  return require.resolve(normalizePath(request), {
+    paths: [process.cwd(originModule.context)]
+  })
+}
 export class WebpackChunkNamePlugin2 implements WebpackPluginInstance {
-  apply(compiler: Compiler) {
+  apply (compiler: Compiler) {
 
     compiler.hooks.compilation.tap(
       'ChunkNamePlugin',
       (compilation) => {
-        console.log(compilation.getModule)
-        compilation.hooks.buildModule.tap(
+        compilation.hooks.succeedModule.tap(
           'ChunkNamePlugin',
           (module) => {
             const resource = module.resource
-            if (resource?.includes('web/pages/index/render.vue?vue&type=script&lang=ts&setup=true')) {
-              console.log(module)
+            if (!resource) {
+              return
             }
-            // const resource = module.resource
-            // if (!resource) {
-            //   return
-            // }
-            // const isPageChunk = checkOrigin(resource)
-            // if (isPageChunk || resource.includes('client-entry')) {
-            //   // if (resource.includes('render.vue')) {
-            //   //   debugger
-            //   //   // console.log('xxxxxxx', module.dependencies)
-            //   // }
-            //   if (resource.includes('web/pages/index/render.vue?vue&type=script&lang=ts&setup=true')) {
-            //     module.dependencies.forEach(item => {
-            //       console.log(item.request)
-            //     })
-            //   }
-            // const { importedIds, dynamicallyImportedIds } = info
-            // const chunkname = id.includes('client-entry') ? 'client-entry' : chunkNameRe.exec(id)![1]
-            // for (const importerId of importedIds) {
-            //   if (!dependenciesMap[importerId]) {
-            //     dependenciesMap[importerId] = []
-            //   }
-            //   dependenciesMap[importerId].push(chunkname)
-            // }
-            // for (const dyImporterId of dynamicallyImportedIds) {
-            //   if (!dependenciesMap[dyImporterId]) {
-            //     dependenciesMap[dyImporterId] = ['dynamic']
-            //   }
-            //   dependenciesMap[dyImporterId].push(chunkname)
-            // }
-          }
+            const pageChunkName = checkOrigin(resource)
+            if (pageChunkName || resource.includes('client-entry')) {
+              const { dependencies } = module
+              const chunkname = resource.includes('client-entry') ? 'client-entry' : pageChunkName
+              for (const d of dependencies) {
+                const importerId = getDependenciesPath(d)
+                if (!dependenciesMap[importerId]) {
+                  dependenciesMap[importerId] = []
+                }
+                dependenciesMap[importerId].push(chunkname)
+              }
+            }
             // else if (dependenciesMap[id]) {
             //   const { importedIds, dynamicallyImportedIds } = this.getModuleInfo(id)!
             //   for (const importerId of importedIds) {
@@ -107,8 +101,8 @@ export class WebpackChunkNamePlugin2 implements WebpackPluginInstance {
             //   }
             // }
           }
+        )
+      }
     )
   }
-    )
-}
 }
