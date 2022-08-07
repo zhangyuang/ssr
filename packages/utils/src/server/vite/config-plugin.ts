cb8b05fe76d07ddf28cd99960314d3da364a3402
zhangyuang <leonin0829@gmail.com>
2022-08-07 20:15:23 +0800

    fix: update example

--- packages/utils/src/server/vite/config-plugin.ts
+++ packages/utils/src/server/vite/config-plugin.ts
@@ -261,6 +261,7 @@ const commonConfig = (): UserConfig => {
         scss: css?.().loaderOptions?.scss ?? {}
       }
     },
+    // @ts-expect-error
     ssr: {
       external: ['ssr-serialize-javascript', 'ssr-server-utils', 'ssr-deepclone', 'ssr-hoc-react', 'ssr-common-utils'].concat(viteConfig?.()?.server?.externals ?? []),
       noExternal: whiteList
