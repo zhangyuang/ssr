fff08efe37436f96e599f2265b1931c0a1d6df72
zhangyuang <leonin0829@gmail.com>
2022-08-22 12:23:10 +0800

    fix: update correct cwd in postinstall

--- packages/utils/src/server/compatible.ts
+++ packages/utils/src/server/compatible.ts
@@ -8,12 +8,13 @@ export function checkForkTs () {
     const framework = judgeServerFramework()
     if (framework === 'ssr-plugin-midway') return
     const cwd = getCwd()
-    const fork = resolve(cwd, './node_modules/fork-ts-checker-webpack-plugin/package.json')
-    const nestCli = resolve(cwd, './node_modules/@nestjs/cli/node_modules')
-    const forkVersion = require(fork).version
+    const userCwd = cwd.slice(0, cwd.indexOf('node_modules'))
+    const fork = resolve(userCwd, './node_modules/fork-ts-checker-webpack-plugin')
+    const forkPackage = resolve(fork, './package.json')
+    const nestCli = resolve(userCwd, './node_modules/@nestjs/cli/node_modules')
+    const forkVersion = require(forkPackage).version
     if (
-      // @ts-expect-error
-      coerce(forkVersion)?.major >= 7 &&
+      coerce(forkVersion)!.major >= 7 &&
       !accessFileSync(resolve(nestCli, './fork-ts-checker-webpack-plugin'))) {
       mv(fork, nestCli)
     }
