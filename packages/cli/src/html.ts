d399beac0dc3a336194651b3fc05f011a7515ff1
zhangyuang <leonin0829@gmail.com>
2022-12-02 00:00:18 +0800

    fix: spa build

--- packages/cli/src/html.ts
+++ packages/cli/src/html.ts
@@ -1,13 +1,12 @@
-// @ts-nocheck
 import { promises } from 'fs'
 import { join } from 'path'
 
 export const generateHtml = async () => {
   if (!process.env.SPA) return
   // spa 模式下生成 html 文件直接部署
-  const { loadConfig, getCwd, judgeFramework, loadModuleFromFramework, logGreen, getAsyncJsChunk, cssOrderPriority } = await import('ssr-common-utils')
+  const { loadConfig, getCwd, judgeFramework, loadModuleFromFramework, logGreen, getAsyncJsChunk, logWarning } = await import('ssr-common-utils')
   logGreen('Generating html file...')
-  const { customeHeadScript, customeFooterScript, hashRouter, htmlTemplate, prefix, clientPrefix, isVite, extraCssOrder } = loadConfig()
+  const { customeHeadScript, customeFooterScript, hashRouter, htmlTemplate, prefix, clientPrefix, isVite, cssOrderPriority, jsOrderPriority } = loadConfig()
   const htmlStr = htmlTemplate ?? `
   <!DOCTYPE html>
   <html lang="en">
@@ -27,7 +26,7 @@ export const generateHtml = async () => {
   </body>
   </html>
   `
-
+  const mockCtx = {} as any
   const framework = judgeFramework()
   let jsHeaderManifest = ''
   let jsFooterManifest = ''
@@ -36,10 +35,10 @@ export const generateHtml = async () => {
   const footer = customeFooterScript ?? []
   const combine = [
     {
-      arr: Array.isArray(header) ? header : header({}),
+      arr: Array.isArray(header) ? header : header(mockCtx),
       flag: 'header'
     }, {
-      arr: Array.isArray(footer) ? footer : footer({}),
+      arr: Array.isArray(footer) ? footer : footer(mockCtx),
       flag: 'footer'
     }]
   combine[0].arr = combine[0].arr.concat([
@@ -68,6 +67,7 @@ export const generateHtml = async () => {
   } if (framework === 'ssr-plugin-vue') {
     for (const item of combine) {
       const { arr, flag } = item
+      // @ts-expect-error
       const scriptArr = arr.map((item) => `<script ${isVite ? 'type="module"' : ''} ${item.describe?.attrs ? `src="${item.describe.attrs.src}" type=text/javascript` : ''}>${item.content} </script>`)
       if (flag === 'header') {
         jsHeaderManifest = scriptArr.join('')
@@ -79,32 +79,37 @@ export const generateHtml = async () => {
 
   const cwd = getCwd()
   const manifest: Record<string, string> = require(join(cwd, './build/client/asset-manifest.json'))
-  let jsManifest = ''
-  const jsOrder = await getAsyncJsChunk({}, '', loadConfig())
+  const jsManifest: string[] = []
+  const jsOrder = await getAsyncJsChunk(mockCtx, '', loadConfig())
   jsOrder.forEach(item => {
     if (manifest[item]) {
-      jsManifest += `<script src="${manifest[item]}" ${isVite ? 'type="module"' : ''}></script>`
+      jsManifest.push(`<script src="${manifest[item]}" ${isVite ? 'type="module"' : ''}></script>`)
     }
   })
-  const cssManifest = []
+  const cssManifest: string[] = []
   Object.values(manifest).reverse().forEach(item => {
     if (item.endsWith('chunk.css')) {
       cssManifest.push(`<link rel='stylesheet' href="${item}" />`)
     }
-  });
-  (extraCssOrder ?? []).forEach(item => {
-    if (manifest?.[item].endsWith('.css')) {
-      cssManifest.push(`<link rel='stylesheet' href="${item}" />`)
-    }
   })
+  if (jsOrderPriority) {
+    const priority = typeof jsOrderPriority === 'function' ? jsOrderPriority({ chunkName: '' }) : jsOrderPriority
+    jsManifest.sort((a, b) => {
+      // 没有显示指定的路由优先级统一为 0
+      return (priority[b] || 0) - (priority[a] || 0)
+    })
+  }
   if (cssOrderPriority) {
-    const priority = typeof cssOrderPriority === 'function' ? cssOrderPriority({ webpackChunkName }) : cssOrderPriority
+    const priority = typeof cssOrderPriority === 'function' ? cssOrderPriority({ chunkName: '' }) : cssOrderPriority
     cssManifest.sort((a, b) => {
       // 没有显示指定的路由优先级统一为 0
       return (priority[b] || 0) - (priority[a] || 0)
     })
   }
-  const generateHtmlStr = htmlStr.replace('cssInject', cssManifest.join('')).replace('jsManifest', jsManifest).replace('jsHeaderManifest', jsHeaderManifest)
+  if (typeof cssOrderPriority === 'function' || typeof jsOrderPriority === 'function') {
+    logWarning('Notices: orderPriority cannot get chunkName in spa html build, you will get chunkName as undefined')
+  }
+  const generateHtmlStr = htmlStr.replace('cssInject', cssManifest.join('')).replace('jsManifest', jsManifest.join('')).replace('jsHeaderManifest', jsHeaderManifest)
     .replace('jsFooterManifest', jsFooterManifest)
     .replace('hashRouterScript', hashRouterScript)
   await promises.writeFile(join(cwd, './build/index.html'), generateHtmlStr)
