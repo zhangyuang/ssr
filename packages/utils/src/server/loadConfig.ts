14a760fce736a42bd432e4372e8021d99f5be6ee
zhangyuang <leonin0829@gmail.com>
2023-02-25 18:07:03 +0800

    ci: update skip ci

--- packages/utils/src/server/loadConfig.ts
+++ packages/utils/src/server/loadConfig.ts
@@ -127,7 +127,7 @@ const loadConfig = (): IConfig => {
     clientOutPut: join(cwd, './build/client'),
     serverOutPut: join(cwd, './build/server')
   })
-  const writeDebounceTime = 1000
+  const writeDebounceTime = 2000
   const webpackDevServerConfig = Object.assign({
     stats: webpackStatsOption,
     disableInfo: true, // 关闭webpack-dev-server 自带的server Info信息
