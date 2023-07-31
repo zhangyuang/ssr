fb877e2718c4b7b8a7381eef83b87096521b7df0
zhangyuang <leonin0829@gmail.com>
2023-07-31 13:57:54 +0800

    chore: update plugin-nestjs tips

--- packages/plugin-nestjs/src/start.ts
+++ packages/plugin-nestjs/src/start.ts
@@ -35,7 +35,7 @@ const start = async (argv: Argv) => {
     }
   })
   stderr?.on('data', function (data) {
-    if (!data.includes('has been deprecated') || !data.includes('reflect-metadata doesn\'t appear to be written in CJS')) {
+    if (!data.includes('has been deprecated') && !data.includes('reflect-metadata doesn\'t appear to be written in CJS')) {
       console.error(`error: ${data}`)
     }
   })
