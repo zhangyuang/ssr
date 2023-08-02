00ad5eabb0384841f45c6dd78c791c8521025694
zhangyuang <leonin0829@gmail.com>
2023-08-02 19:08:30 +0800

    feat: add webpack error plugin emit error close #304

--- packages/webpack/src/utils/promisify.ts
+++ packages/webpack/src/utils/promisify.ts
@@ -1,7 +1,27 @@
-import { promisify } from 'util'
 import * as webpack from 'webpack'
+import { Stats, Configuration, Compiler } from 'webpack'
 
-const webpackPromisify = promisify<webpack.Configuration, webpack.Stats>(webpack)
+const errorEmitPlugin = function (compiler: Compiler) {
+  compiler.hooks.done.tapAsync('done', function (stats, callback) {
+    if (stats.compilation.errors.length > 0) {
+      throw new Error(
+        JSON.stringify(stats.compilation.errors.map(err => err.message || err))
+      )
+    }
+    callback()
+  })
+}
+const webpackPromisify = async (config: Configuration): Promise<Stats> => {
+  config.plugins?.push(errorEmitPlugin)
+  return await new Promise((resolve, reject) => {
+    webpack(config, (err, stats: Stats) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(stats)
+    })
+  })
+}
 
 export {
   webpackPromisify
