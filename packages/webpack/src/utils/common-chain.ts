c388a465672d2bd7c457573fa6c0a64605263f35
zhangyuang <leonin0829@gmail.com>
2025-07-26 01:51:44 +0800

    ci: run

--- packages/webpack/src/utils/common-chain.ts
+++ packages/webpack/src/utils/common-chain.ts
@@ -4,7 +4,7 @@ import type { Module, Rule } from 'webpack-chain'
 import { promises } from 'fs'
 import { resolve } from 'path'
 import * as webpack from 'ssr-webpack4'
-import { asyncChunkMap, getCwd, judgeFramework, loadModuleFromFramework, loadModuleFromWebpack, getPkgMajorVersion, loadConfig, logWarning, getImageOutputPath, nameSpaceBuiltinModules, getBuildConfig, getDefineEnv } from 'ssr-common-utils'
+import { asyncChunkMap, getCwd, judgeFramework, loadModuleFromFramework, loadModuleFromWebpack, getPkgMajorVersion, loadConfig, logWarning, getImageOutputPath, nameSpaceBuiltinModules, getBuildConfig, getDefineEnv, isReact18 } from 'ssr-common-utils'
 import { nodeExternals } from './externals'
 import { FileToChunkRelationPlugin } from './plugins'
 import { setStyle } from './setStyle'
@@ -286,6 +286,13 @@ const addCommonChain = (chain: Chain, isServer: boolean) => {
 		}
 	])
 
+	if (!isReact18() && judgeFramework() === 'ssr-plugin-react') {
+		chain.plugin('ignore-plugin').use(
+			new webpack.IgnorePlugin({
+				resourceRegExp: /^react-dom\/client$/
+			})
+		)
+	}
 	if (!isServer) {
 		nameSpaceBuiltinModules.forEach((moduleName) => {
 			chain.node.set(moduleName, 'empty')
