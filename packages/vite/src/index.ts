a111660cfb2557c44415ec5eae56fc1160d87ced
zhangyuang <leonin0829@gmail.com>
2025-07-27 01:18:11 +0800

    ci: run

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -4,7 +4,7 @@ import type * as ReactPlugin from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import babel from '@rollup/plugin-babel'
 import { visualizer } from 'rollup-plugin-visualizer'
-import { getCwd, getDefineEnv, getOutputPublicPath, loadConfig, loadModuleFromFramework, judgeFramework, accessFileSync, getBuildEntry } from 'ssr-common-utils'
+import { getCwd, getDefineEnv, getOutputPublicPath, loadConfig, loadModuleFromFramework, judgeFramework, accessFileSync, getBuildEntry, isReact18 } from 'ssr-common-utils'
 import { UserConfig, build as viteBuild, PluginOption } from 'vite'
 
 import { AndDesignVueResolve, AntdResolve, ElementPlusResolve, NutuiResolve, VantResolve, createStyleImportPlugin } from 'ssr-vite-plugin-style-import'
@@ -15,7 +15,7 @@ const framework = judgeFramework()
 const isReact = framework === 'ssr-plugin-react'
 const isVue3 = framework === 'ssr-plugin-vue3'
 const hasReactIs = accessFileSync(resolve(getCwd(), './node_modules/react-is'))
-const extraInclude = [''].concat(isReact ? ['react', 'react-dom/client', 'react-dom', 'react-router', 'react-router-dom', hasReactIs ? 'react-is' : ''] : []).filter(Boolean)
+const extraInclude = [''].concat(isReact ? ['react', isReact18() ? 'react-dom/client' : 'react-dom', 'react-router', 'react-router-dom', hasReactIs ? 'react-is' : ''] : []).filter(Boolean)
 const extraExclude = ['ssr-hoc-react']
 
 const { getOutput, viteConfig, supportOptinalChaining, isDev, define, optimize, babelOptions, chunkName } = loadConfig()
