1605ff883ff746e68de09c7b62408795bbccf041
zhangyuang <leonin0829@gmail.com>
2025-08-05 14:18:26 +0800

    docs: update docs.md

--- docs/web/pages/docs/fetch.ts
+++ docs/web/pages/docs/fetch.ts
@@ -9,7 +9,7 @@ export default async ({ router }, ctx?: ISSRContext) => {
     if (path === 'features/v7') {
       document.title = 'ssr框架v7发布，全网首个同时支持Rspack,Rolldown-Vite,Webpack的ssr框架'
     } else {
-      document.title = 'ssr framework'
+      document.title = 'ssr 框架官方文档'
     }
   }
   return {
