f9ac5662e138e9d8e6e537e69ccb45d736d66870
zhangyuang <leonin0829@gmail.com>
2025-09-03 19:32:44 +0800

    feat: update webpack split-chunk

--- packages/webpack/src/utils/split-chunk.ts
+++ packages/webpack/src/utils/split-chunk.ts
@@ -24,39 +24,57 @@ export const getSplitChunksOptions = () => {
 const getModuleName = (module: NormalModule) => {
 	return module.resource?.split('?')[0] || ''
 }
+const directChunkModules = [] as NormalModule[]
 
 export class splitChunkPlugin {
 	apply(compiler: Webpack.Compiler) {
 		compiler.hooks.compilation.tap('splitChunkPlugin', (compilation) => {
-			compilation.hooks.finishModules.tap('splitChunkPlugin', (normalModules) => {
+			compilation.hooks.afterOptimizeModules.tap('splitChunkPlugin', (normalModules) => {
 				for (const module of normalModules) {
 					const normalModule = module as unknown as NormalModule
 					const modulePath = getModuleName(normalModule)
 					if (!modulePath) {
 						continue
 					}
+					if (normalModule.resource?.includes('chunkName')) {
+						directChunkModules.push(module as unknown as NormalModule)
+					}
+					if (normalModule.resource?.includes('client-entry')) {
+						directChunkModules.push(module as unknown as NormalModule)
+					}
+				}
 
-					const incomings = module.reasons.map((r) => r.module as NormalModule).filter(Boolean)
-					const chunkNames = normalModule.resource?.includes('chunkName')
-						? [chunkNameRe.exec(normalModule.resource ?? '')?.[1]]
-						: incomings
-								.map((c) => {
-									if (c.resource?.includes('client-entry')) {
-										return 'Page'
-									}
-									return c.resource?.includes('chunkName')
-										? chunkNameRe.exec(c.resource ?? '')?.[1]
-										: dependenciesMap[c.resource?.split('?')[0] ?? '']
-								})
-								.flat()
-								.filter(Boolean)
-					dependenciesMap[modulePath] = dependenciesMap[modulePath]
-						? dependenciesMap[modulePath].concat(chunkNames as string[])
-						: (chunkNames as string[])
-					dependenciesMap[modulePath] = Array.from(new Set(dependenciesMap[modulePath]))
+				for (const directChunkModule of directChunkModules) {
+					const queue = [directChunkModule]
+					const visited = new Set<string>()
+					const moduleChunkName = chunkNameRe.exec(directChunkModule.resource ?? '')?.[1]! || 'Page'
+					while (queue.length) {
+						const currentModule = queue.shift()
+						const modulePath = getModuleName(currentModule as unknown as NormalModule)
+						if (!modulePath || modulePath.includes('css-loader')) {
+							continue
+						}
+						dependenciesMap[modulePath] = dependenciesMap[modulePath]
+							? dependenciesMap[modulePath].concat(moduleChunkName)
+							: [moduleChunkName]
+						const outgoings = currentModule!.dependencies ?? []
+						for (const outgoing of outgoings) {
+							//@ts-expect-error
+							const modulePath = outgoing.module?.resource
+							if (!modulePath) {
+								continue
+							}
+							if (visited.has(modulePath)) {
+								continue
+							}
+							visited.add(modulePath)
+							//@ts-expect-error
+							queue.push(outgoing.module as NormalModule)
+						}
+					}
 				}
 				for (const fileName in dependenciesMap) {
-					let chunkNames = dependenciesMap[fileName]
+					let chunkNames = Array.from(new Set(dependenciesMap[fileName]))
 					if (fileName.includes('node_modules')) {
 						chunkNames.push('vendor')
 					}
@@ -68,7 +86,6 @@ export class splitChunkPlugin {
 					if (chunkNames.includes('Page')) {
 						chunkNames = chunkNames.includes('vendor') ? ['Page', 'vendor'] : ['Page']
 					}
-					chunkNames = Array.from(new Set(chunkNames))
 					dependenciesMap[fileName] = chunkNames
 					generateMap[fileName] = dependenciesMap[fileName].join('~')
 					asyncChunkMap[generateMap[fileName]] = chunkNames
@@ -83,7 +100,11 @@ export class splitChunkPlugin {
 const getWebpackSplitCache = (): OptimizationSplitChunksOptions['cacheGroups'] => {
 	return {
 		dynamicChunks: {
-			test: (module) => !!(module as NormalModule).resource,
+			test: (module) => {
+				const normalModule = module as NormalModule
+				const moduleName = normalModule.resource?.split('?')?.[0]
+				return !!generateMap[moduleName ?? '']
+			},
 			name: (module) => {
 				const normalModule = module as NormalModule
 				const moduleName = normalModule.resource?.split('?')?.[0]
